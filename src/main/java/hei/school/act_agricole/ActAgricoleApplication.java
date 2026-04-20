afc6ab4e403c52c53765437d014abac88ca9d74a
Baby055 <hei.sombiniaina@gmail.com>
2026-04-20 09:07:12 +0300

    initialisation

---
+++ src/main/java/hei/school/act_agricole/ActAgricoleApplication.java
@@ -0,0 +1,13 @@
+package hei.school.act_agricole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActAgricoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActAgricoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/act_agricole/ActAgricoleApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.act_agricole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActAgricoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
