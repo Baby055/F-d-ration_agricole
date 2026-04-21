9364cdc913c9549b94635e2d4b0fde68e5421094
Carine Rasamoely <hei.bryan.17@gmail.com>
2026-04-21 20:01:13 +0300

    dto/CreateCollectivityRequest : fini

--- src/main/java/hei/school/act_agricole/dto/request/CreateCollectivityRequest.java
+++ src/main/java/hei/school/act_agricole/dto/request/CreateCollectivityRequest.java
@@ -1,5 +1,20 @@
 package hei.school.act_agricole.dto.request;
 
+import java.util.List;
+
 public class CreateCollectivityRequest {
+   private String location;
+    private List<String> members;
+    private boolean federationApproval;
+    private CreateCollectivityStructureRequest structure;
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+    public List<String> getMembers() { return members; }
+    public void setMembers(List<String> members) { this.members = members; }
+    public boolean isFederationApproval() { return federationApproval; }
+    public void setFederationApproval(boolean federationApproval) { this.federationApproval = federationApproval; }
+    public CreateCollectivityStructureRequest getStructure() { return structure; }
+    public void setStructure(CreateCollectivityStructureRequest structure) { this.structure = structure; }
 
 }
