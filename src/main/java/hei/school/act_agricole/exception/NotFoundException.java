f9aba2010f4583c78840fe5d9d51c5ef5bc0892f
Baby055 <hei.sombiniaina@gmail.com>
2026-04-21 21:13:21 +0300

    exception/all : gestion des erreurs

---
+++ src/main/java/hei/school/act_agricole/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package hei.school.act_agricole.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
