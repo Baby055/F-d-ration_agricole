ed30eae8ec8d248e491bc2882a309145a11018b6
Baby055 <hei.sombiniaina@gmail.com>
2026-04-21 16:05:29 +0300

    entity/Member  : ajout du classe Member

---
+++ src/main/java/hei/school/act_agricole/entity/Member.java
@@ -0,0 +1,108 @@
+package hei.school.act_agricole.entity;
+
+import hei.school.act_agricole.enums.Gender;
+import hei.school.act_agricole.enums.MemberOccupation;
+
+import java.time.LocalDate;
+
+public class Member {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String address;
+    private String profession;
+    private String phoneNumber;
+    private String email;
+    private MemberOccupation occupation;
+    private LocalDate federationJoiningDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getFederationJoiningDate() {
+        return federationJoiningDate;
+    }
+
+    public void setFederationJoiningDate(LocalDate federationJoiningDate) {
+        this.federationJoiningDate = federationJoiningDate;
+    }
+
+    public MemberOccupation getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(MemberOccupation occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
