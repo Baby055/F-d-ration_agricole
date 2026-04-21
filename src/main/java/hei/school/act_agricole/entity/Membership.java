71ecc1b7eeeaed330df59a7f31544e909678f349
Baby055 <hei.sombiniaina@gmail.com>
2026-04-21 16:35:54 +0300

    entity/Membership.java : fini

---
+++ src/main/java/hei/school/act_agricole/entity/Membership.java
@@ -0,0 +1,51 @@
+package hei.school.act_agricole.entity;
+
+import java.time.LocalDate;
+
+public class Membership {
+    private String id;
+    private String memberId;
+    private String collectivityId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCollectivityId() {
+        return collectivityId;
+    }
+
+    public void setCollectivityId(String collectivityId) {
+        this.collectivityId = collectivityId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
