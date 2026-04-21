bdf54c6048f67c67ef3f9aeb9a051c8d7198ff3f
Baby055 <hei.sombiniaina@gmail.com>
2026-04-21 16:42:58 +0300

    entity/Mandate.java : modification

--- src/main/java/hei/school/act_agricole/entity/Mandate.java
+++ src/main/java/hei/school/act_agricole/entity/Mandate.java
@@ -13,38 +13,48 @@ public class Mandate {
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
-    public String getMemberId() {
-        return memberId;
-    }
-    public void setMemberId(String memberId) {
-        this.memberId = memberId;
-    }
+
     public String getCollectivityId() {
         return collectivityId;
     }
+
     public void setCollectivityId(String collectivityId) {
         this.collectivityId = collectivityId;
     }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
+
     public LocalDate getStartDate() {
         return startDate;
     }
+
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
+
     public LocalDate getEndDate() {
         return endDate;
     }
+
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
-
 }
