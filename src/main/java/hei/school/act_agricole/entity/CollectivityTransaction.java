25af693cd05181725dbeb8d577ea596493f8f49d
Baby055 <hei.sombiniaina@gmail.com>
2026-04-22 15:35:01 +0300

    entity/CollectivityTransaction : fini

---
+++ src/main/java/hei/school/act_agricole/entity/CollectivityTransaction.java
@@ -0,0 +1,69 @@
+package hei.school.act_agricole.entity;
+
+import java.time.LocalDate;
+
+public class CollectivityTransaction {
+    private String id;
+    private String collectivityId;
+    private LocalDate creationDate;
+    private double amount;
+    private PaymentMode paymentMode;
+    private String accountCreditedId;
+    private String memberDebitedId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCollectivityId() {
+        return collectivityId;
+    }
+
+    public void setCollectivityId(String collectivityId) {
+        this.collectivityId = collectivityId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getAccountCreditedId() {
+        return accountCreditedId;
+    }
+
+    public void setAccountCreditedId(String accountCreditedId) {
+        this.accountCreditedId = accountCreditedId;
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public String getMemberDebitedId() {
+        return memberDebitedId;
+    }
+
+    public void setMemberDebitedId(String memberDebitedId) {
+        this.memberDebitedId = memberDebitedId;
+    }
+}
