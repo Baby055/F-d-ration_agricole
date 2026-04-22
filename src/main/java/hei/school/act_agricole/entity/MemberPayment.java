e7863ac446c5d677bc66cbb4865f3d44aff57705
Baby055 <hei.sombiniaina@gmail.com>
2026-04-22 15:44:32 +0300

    entity/MemberPayment : fini

---
+++ src/main/java/hei/school/act_agricole/entity/MemberPayment.java
@@ -0,0 +1,69 @@
+package hei.school.act_agricole.entity;
+
+import java.time.LocalDate;
+
+public class MemberPayment {
+    private String id;
+    private String memberId;
+    private String membershipFeeId;
+    private double amount;
+    private PaymentMode paymentMode;
+    private String transactionId;
+    private LocalDate creationDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMembershipFeeId() {
+        return membershipFeeId;
+    }
+
+    public void setMembershipFeeId(String membershipFeeId) {
+        this.membershipFeeId = membershipFeeId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+}
