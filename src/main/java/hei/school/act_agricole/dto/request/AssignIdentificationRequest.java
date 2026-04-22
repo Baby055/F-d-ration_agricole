2185076940612d5b1cac83392a677a2c3411e9bc
Baby055 <hei.sombiniaina@gmail.com>
2026-04-22 09:02:58 +0300

    dto/request/Collectivity.java : fini

---
+++ src/main/java/hei/school/act_agricole/dto/request/AssignIdentificationRequest.java
@@ -0,0 +1,22 @@
+package hei.school.act_agricole.dto.request;
+
+public class AssignIdentificationRequest {
+    private String number;
+    private String name;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
