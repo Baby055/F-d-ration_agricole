f4f368ab4b7c0831246a5ed928014df91d45b47c
Baby055 <hei.sombiniaina@gmail.com>
2026-05-07 00:13:14 +0300

    controller/TransactionController.java : ajout d'une endpoint

--- src/main/java/hei/school/act_agricole/controller/TransactionController.java
+++ src/main/java/hei/school/act_agricole/controller/TransactionController.java
@@ -1,15 +1,15 @@
 package hei.school.act_agricole.controller;
 
 import hei.school.act_agricole.dto.request.CreateMemberPaymentRequest;
+import hei.school.act_agricole.dto.response.CollectivityTransactionResponse;
 import hei.school.act_agricole.dto.response.MemberPaymentResponse;
 import hei.school.act_agricole.service.TransactionService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -27,5 +27,14 @@ public class TransactionController {
             @RequestBody List<CreateMemberPaymentRequest> requests) {
         List<MemberPaymentResponse> responses = service.createPayments(id, requests);
         return ResponseEntity.status(HttpStatus.CREATED).body(responses);
+    }
+
+    @GetMapping("/collectivities/{id}/transactions")
+    public ResponseEntity<List<CollectivityTransactionResponse>> getTransactions(
+            @PathVariable String id,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+        List<CollectivityTransactionResponse> responses = service.getTransactions(id, from, to);
+        return ResponseEntity.ok(responses);
     }
 }
